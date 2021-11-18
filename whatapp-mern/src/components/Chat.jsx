e15ecb37b8e9f726eea1476c7ee76afb10a3ff96
Arman Ali <toplinegeeks@gmail.com>
2021-11-18 16:38:27 +0500

    create a footer component and styling

--- whatapp-mern/src/components/Chat.jsx
+++ whatapp-mern/src/components/Chat.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./chat.css";
-import { SearchOutlined, AttachFile, MoreVert } from "@material-ui/icons";
+import {
+  SearchOutlined,
+  AttachFile,
+  MoreVert,
+  InsertEmoticon,
+  Mic,
+} from "@material-ui/icons";
 import { Avatar, IconButton } from "@material-ui/core";
 import { ChatMessage } from "./ChatMessage.jsx";
 import { ChatReciever } from "./ChatReciever";
@@ -27,11 +33,22 @@ export const Chat = () => {
           </IconButton>
         </div>
       </div>
+      {/* chat body */}
       <div className="chat__body">
         <ChatMessage />
         <ChatMessage />
         <ChatMessage />
         <ChatReciever />
+      </div>
+
+      {/* chat footer */}
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input type="text" placeholder="Type Message here" />
+          <button type="submit"> Send a Message</button>
+        </form>
+        <Mic />
       </div>
     </div>
   );
