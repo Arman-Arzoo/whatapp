ae1fd55dcc4e1d8c527a7854283d7a47b80e5a56
Arman Ali <toplinegeeks@gmail.com>
2021-11-18 12:51:44 +0500

    left side icon for sidebar component

--- whatapp-mern/src/components/Sidebar.jsx
+++ whatapp-mern/src/components/Sidebar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./sidebar.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import { IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { Avatar, IconButton } from "@material-ui/core";
 
 export const Sidebar = () => {
   return (
@@ -10,6 +12,14 @@ export const Sidebar = () => {
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
+          </IconButton>
+
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
           </IconButton>
         </div>
       </div>
